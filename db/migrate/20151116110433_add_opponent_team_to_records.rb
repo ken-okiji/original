b2e68478c46ee9c241783fc2e79aa2a519091ef5
ken_okiji <no_sweet_without_sweat@i.softbank.jp>
2015-11-21 14:21:45 +0000

    original

--- db/migrate/20151116110433_add_opponent_team_to_records.rb
+++ db/migrate/20151116110433_add_opponent_team_to_records.rb
@@ -1,5 +1,6 @@
 class AddOpponentTeamToRecords < ActiveRecord::Migration
   def change
+    add_column :records, :opponent_team, :string
     add_column :records, :score, :string
     add_column :records, :goal, :integer
     add_column :records, :assist, :integer
