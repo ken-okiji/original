35bd3afd30ac528003672f4732d5fa9ee26171ad
ken_okiji <no_sweet_without_sweat@i.softbank.jp>
2015-11-21 14:40:17 +0000

    original

--- db/migrate/20151109134501_add_username_to_users.rb
+++ db/migrate/20151109134501_add_username_to_users.rb
@@ -1,5 +1,10 @@
 class AddUsernameToUsers < ActiveRecord::Migration
   def change
     add_column :users, :username, :string
+    add_column :users, :area, :string
+    add_column :users, :age, :integer
+    add_column :users, :playername, :string
+    add_column :users, :team, :string
+    add_column :users, :position, :string
   end
 end
--- db/migrate/20151116110433_add_opponent_team_to_records.rb
+++ db/migrate/20151116110433_add_opponent_team_to_records.rb
@@ -1,8 +1,12 @@
 class AddOpponentTeamToRecords < ActiveRecord::Migration
   def change
     add_column :records, :opponent_team, :string
+    add_column :records, :date, :string
     add_column :records, :score, :string
     add_column :records, :goal, :integer
     add_column :records, :assist, :integer
+    add_column :records, :participation_time, :integer
+    add_column :records, :redcard, :integer
+    add_column :records, :yellowcard, :integer
   end
 end
